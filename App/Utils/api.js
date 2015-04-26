edf8194f6de1bed3ede497164c95a1f09fdf01d4
Arvin Randhawa <arvinrandhawa@gmail.com>
2015-04-26 18:14:23 -0500

    Wrote getNote and addNote methods to Api.js using Firebase

--- App/Utils/api.js
+++ App/Utils/api.js
@@ -8,7 +8,20 @@ var api = {
 		username = username.toLowerCase().trim();
 		var url = `https://api.github.com/users/${username}/repos`;
 		return fetch(url).then((res) => res.json());
-	}
+	},
+	getNotes(username){
+		username = username.toLowerCase().trim();
+		var url = `https://egghead-react-native.firebaseio.com/${username}.json`
+		return fetch(url).then((res) => res.json());
+	},
+	addNote(username, note){
+		username = username.toLowerCase().trim();
+		var url = `https://egghead-react-native.firebaseio.com/${username}.json`
+		return fetch(url, {
+			method: 'post',
+			body: JSON.stringify(note)
+		}).then((res) => res.json());
+	},
 };
 
 
